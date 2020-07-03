6fa0ae46d1e25efd75c7860391480fc1631f2a3f
nrx <ageevav97@gmail.com>
2020-07-03 20:44:14 +0300

    исправлено предупреждение и поправлены travis с cmake

--- include/repository/PositionRepository.h
+++ include/repository/PositionRepository.h
@@ -14,6 +14,7 @@ public:
     virtual std::list<Position> findByTitle(const std::string &title) = 0;
     virtual std::list<Position> getAll() = 0;
     virtual void deletePosition(const Position &position) = 0;
+    virtual ~PositionRepository() = default;
 };
 
 #endif //ABSTRACT_POSITIONS_POSITIONREPOSITORY_H
